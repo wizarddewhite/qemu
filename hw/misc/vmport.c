34e3ae9522d64184dac6f975e112f1ebb93704aa
Don Slutz <Don@CloudSwitch.com>
2012-08-31 13:20:23 -0400

    vmport: Add VMware's GETHZ command.
    This command is known is linux as VMWARE_PORT_CMD_GETHZ. It is required to make
    VMware ESX detect a valid TSC frequency on bootup.
    
    Signed-off-by: Don Slutz <Don@CloudSwitch.com>
    [agraf: Rewrite to correctly pass on real hw TSC]
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/misc/vmport.c
+++ hw/misc/vmport.c
@@ -31,8 +31,9 @@
 
 #define VMPORT_CMD_GETVERSION 0x0a
 #define VMPORT_CMD_GETRAMSIZE 0x14
+#define VMPORT_CMD_GETHZ      0x2d
 
-#define VMPORT_ENTRIES 0x2c
+#define VMPORT_ENTRIES 0x2e
 #define VMPORT_MAGIC   0x564D5868
 
 #define TYPE_VMPORT "vmport"
@@ -112,6 +113,50 @@ static uint32_t vmport_cmd_ram_size(void *opaque, uint32_t addr)
     return ram_size;
 }
 
+#ifndef __x86_64__
+#error XXX code not adapted to non-x86-64
+#endif
+static inline uint64_t rdtsc(void)
+{
+    uint32_t lo, hi;
+    uint64_t r;
+    /* We cannot use "=A", since this would use %rax on x86_64 */
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    r = ((uint64_t)hi << 32) | lo;
+    return r;
+}
+
+static uint32_t vmport_cmd_get_hz(void *opaque, uint32_t addr)
+{
+    X86CPU *cpu = X86_CPU(current_cpu);
+    CPUX86State *env = &cpu->env;
+    uint64_t tsc_hz;
+    const uint64_t apic_hz = 660000000L;
+
+    tsc_hz = (uint64_t)env->tsc_khz;
+    if  (!tsc_hz) {
+        /* Need to measure locally */
+        static uint64_t tsc = 0;
+        if ( tsc == 0 ) {
+            tsc = rdtsc();
+            usleep(100000);
+            tsc = (rdtsc() - tsc) * 10;
+        }
+        tsc_hz = tsc;
+    }
+
+    /* Round down to bus frequency */
+    tsc_hz = (tsc_hz / apic_hz) * apic_hz;
+
+    /* apic-frequency (bus speed) */
+    env->regs[R_ECX] = apic_hz;
+    /* High part of tsc-frequency */
+    env->regs[R_EBX] = (uint32_t)(tsc_hz >> 32);
+    /* Low part of tsc-frequency */
+    env->regs[R_EAX] = (uint32_t)tsc_hz;
+    return (uint32_t)tsc_hz;
+}
+
 /* vmmouse helpers */
 void vmmouse_get_data(uint32_t *data)
 {
@@ -155,6 +200,7 @@ static void vmport_realizefn(DeviceState *dev, Error **errp)
     /* Register some generic port commands */
     vmport_register(VMPORT_CMD_GETVERSION, vmport_cmd_get_version, NULL);
     vmport_register(VMPORT_CMD_GETRAMSIZE, vmport_cmd_ram_size, NULL);
+    vmport_register(VMPORT_CMD_GETHZ, vmport_cmd_get_hz, NULL);
 }
 
 static void vmport_class_initfn(ObjectClass *klass, void *data)
