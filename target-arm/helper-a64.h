8d7ef0a5618f70c53c20949231cfbd9a83e7290f
Alexander Graf <agraf@suse.de>
2013-09-23 10:58:30 +0200

    AArch64: Add orr instruction emulation
    This patch adds emulation support for the orr instruction.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-arm/helper-a64.h
+++ target-arm/helper-a64.h
@@ -16,3 +16,5 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
+
+DEF_HELPER_FLAGS_4(pstate_add, TCG_CALL_NO_RWG_SE, i32, i32, i64, i64, i64)
