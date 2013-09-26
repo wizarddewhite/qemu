837248cf7c61018de09602094c7728e2db756335
Alexander Graf <agraf@suse.de>
2013-09-26 14:38:00 +0200

    AArch64: Add ins GPR->Vec instruction emulation
    This patch adds emulation for the INS instruction flavor that copies
    GPR contents into vector register parts.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    
    ---
    
    v2 -> v3:
    
      - use extract32/sextract32 instead of get_bits and get_sbits

--- target-arm/translate-a64.c
+++ target-arm/translate-a64.c
@@ -918,6 +918,42 @@ static void handle_umov(DisasContext *s, uint32_t insn)
     }
 }
 
+static void handle_insg(DisasContext *s, uint32_t insn)
+{
+    int rd = extract32(insn, 0, 5);
+    int rn = extract32(insn, 5, 5);
+    int imm5 = extract32(insn, 16, 6);
+    int freg_offs_d = offsetof(CPUARMState, vfp.regs[rd * 2]);
+    int size;
+    int idx;
+
+    for (size = 0; !(imm5 & (1 << size)); size++) {
+        if (size > 3) {
+            unallocated_encoding(s);
+            return;
+        }
+    }
+
+    switch (size) {
+    case 0:
+        idx = extract32(imm5, 1, 4) << 0;
+        tcg_gen_st8_i64(cpu_reg(rn), cpu_env, freg_offs_d + idx);
+        break;
+    case 1:
+        idx = extract32(imm5, 2, 3) << 1;
+        tcg_gen_st16_i64(cpu_reg(rn), cpu_env, freg_offs_d + idx);
+        break;
+    case 2:
+        idx = extract32(imm5, 3, 2) << 2;
+        tcg_gen_st32_i64(cpu_reg(rn), cpu_env, freg_offs_d + idx);
+        break;
+    case 3:
+        idx = extract32(imm5, 4, 1) << 3;
+        tcg_gen_st_i64(cpu_reg(rn), cpu_env, freg_offs_d + idx);
+        break;
+    }
+}
+
 void disas_a64_insn(CPUARMState *env, DisasContext *s)
 {
     uint32_t insn;
@@ -983,6 +1019,9 @@ void disas_a64_insn(CPUARMState *env, DisasContext *s)
         } else if (!extract32(insn, 31, 1) && !extract32(insn, 29, 1) &&
             (extract32(insn, 10, 6) == 0xf)) {
             handle_umov(s, insn);
+        } else if ((extract32(insn, 29, 3) == 2) && !extract32(insn, 21, 3) &&
+            (extract32(insn, 10, 6) == 0x7)) {
+            handle_insg(s, insn);
         } else {
             unallocated_encoding(s);
         }
