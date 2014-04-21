2f60ba305ae09c056a9396b8451d693881010dfc
Tom Musta <tommusta@gmail.com>
2014-04-21 15:55:19 -0500

    target-ppc: Introduce DFP Extract Biased Exponent
    Add emulation of the PowerPC Decimal Floating Point Extract
    Biased Exponent instructions dxex[q][.].
    
    Signed-off-by: Tom Musta <tommusta@gmail.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-ppc/helper.h
+++ target-ppc/helper.h
@@ -660,4 +660,6 @@ DEF_HELPER_4(ddedpd, void, env, fprp, fprp, i32)
 DEF_HELPER_4(ddedpdq, void, env, fprp, fprp, i32)
 DEF_HELPER_4(denbcd, void, env, fprp, fprp, i32)
 DEF_HELPER_4(denbcdq, void, env, fprp, fprp, i32)
+DEF_HELPER_3(dxex, void, env, fprp, fprp)
+DEF_HELPER_3(dxexq, void, env, fprp, fprp)
 #include "exec/def-helper.h"
